ef874c974d38e3fdbf3b02c803f2f8e00db77e0f
Levi Eggert <levi.eggert@cru.org>
2023-12-29 15:06:48 -0500

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WeatherAppKotlin",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "WeatherAppKotlin",
+            targets: ["WeatherAppKotlin"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "WeatherAppKotlin"),
+        .testTarget(
+            name: "WeatherAppKotlinTests",
+            dependencies: ["WeatherAppKotlin"]),
+        .binaryTarget(name: "WeatherAppKotlinBinaryPackage", path: "XCFrameworks/release/shared.xcframework")
+    ]
+)
