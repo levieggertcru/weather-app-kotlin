311b63965419cad64c7b359dea16413b085f9b29
Levi Eggert <levi.eggert@cru.org>
2023-12-29 15:36:35 -0500

    Remove dsyms Add binary package to target

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WeatherAppKotlin",
-            targets: ["WeatherAppKotlin"]),
+            targets: ["WeatherAppKotlin", "WeatherAppKotlinBinaryPackage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
