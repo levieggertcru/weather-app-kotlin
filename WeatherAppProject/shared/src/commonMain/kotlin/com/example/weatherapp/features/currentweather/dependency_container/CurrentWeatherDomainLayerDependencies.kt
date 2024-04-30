82293137fd1b02507f7dd15e3936a5a6e2bcc255
Levi Eggert <levi.eggert@cru.org>
2024-04-30 09:05:41 -0400

    Add GetSearchedWeatherUseCase to current weather domain layer dependencies

--- WeatherAppProject/shared/src/commonMain/kotlin/com/example/weatherapp/features/currentweather/dependency_container/CurrentWeatherDomainLayerDependencies.kt
+++ WeatherAppProject/shared/src/commonMain/kotlin/com/example/weatherapp/features/currentweather/dependency_container/CurrentWeatherDomainLayerDependencies.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp.features.currentweather.dependency_container
 
 import com.example.weatherapp.features.currentweather.domain.usecases.GetCurrentWeatherUseCase
+import com.example.weatherapp.features.currentweather.domain.usecases.GetSearchedWeatherUseCase
 
 class CurrentWeatherDomainLayerDependencies(
     private val dataLayer: CurrentWeatherDataLayerDependencies
@@ -10,5 +11,9 @@ class CurrentWeatherDomainLayerDependencies(
         return GetCurrentWeatherUseCase(
             dataLayer.getCurrentWeatherRepository()
         )
+    }
+
+    fun getSearchedWeatherUseCase(): GetSearchedWeatherUseCase {
+        return GetSearchedWeatherUseCase()
     }
 }
